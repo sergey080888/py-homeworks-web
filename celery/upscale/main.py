a6ee7889a0394beced077b011f4171271fb847cb
Сергей Ковальский <237264@mail.ru>
2023-03-07 12:29:49 +0300

    первый комит

---
+++ celery/upscale/main.py
@@ -0,0 +1,10 @@
+from upscale import example
+
+
+def main():
+
+    example.delay().get()
+
+
+if __name__ == '__main__':
+    main()
--- celery/upscale/upscale.py
+++ celery/upscale/upscale.py
@@ -1,5 +1,9 @@
 import cv2
 from cv2 import dnn_superres
+from celery import Celery
+
+
+celery_app = Celery(broker='redis://127.0.0.1:6379/1', backend='redis://127.0.0.1:6379/2')
 
 
 def upscale(input_path: str, output_path: str, model_path: str = 'EDSR_x2.pb') -> None:
@@ -18,9 +22,11 @@ def upscale(input_path: str, output_path: str, model_path: str = 'EDSR_x2.pb') -
     cv2.imwrite(output_path, result)
 
 
+
+@celery_app.task
 def example():
     upscale('lama_300px.png', 'lama_600px.png')
 
 
-if __name__ == '__main__':
-    example()
+# if __name__ == '__main__':
+#     example()
